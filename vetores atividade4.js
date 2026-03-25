a4ecbc15e1c298cb26e02bc60740edf07ce24f49
luan2021viver-del <luan2021viver@gmail.com>
2026-03-25 18:51:23 -0300

    Add files via upload

---
+++ vetores atividade4.js
@@ -0,0 +1,11 @@
+let num = [8,7,9]
+
+if(num[0]+[1]+[2]>= 21) {
+    console.log("aprovado")
+}
+else if (num[0]+[1]+[2]<= 21) {
+    console.log("reprovado")
+}
+if (num[0]+[1]+[2]<= 0) {  
+    console.log("recuperação")
+}
