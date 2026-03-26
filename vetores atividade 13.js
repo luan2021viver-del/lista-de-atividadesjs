8672b6dac469de9b1c9e9c855fbd82735be3657a
luan2021viver-del <luan2021viver@gmail.com>
2026-03-25 21:18:17 -0300

    Add files via upload

---
+++ vetores atividade 13.js
@@ -0,0 +1,7 @@
+let num = [5,12, 8, 20,3];
+
+//FILTRA numeros maiores que 10
+
+let Maior = num.filter( num => num >= 10)
+
+console.log(Maior);
---
+++ vetores atividade 14.js
@@ -0,0 +1,7 @@
+let nome = ["ANA","Bruno","Carlos","Eva"];
+
+//FILTRA nomes no string maior que 10
+
+let Nomes4 = nome.filter(nome => nome.lenght > 4)
+
+console.log(Nomes4);
