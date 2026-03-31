cbb87db8e05c72486fe4f88ad6d0664f72157feb
luan2021viver-del <luan2021viver@gmail.com>
2026-03-30 21:33:40 -0300

    Add files via upload

---
+++ atividade7.js
@@ -0,0 +1,7 @@
+let nomes = []
+for (let i = 0; i < 5; i++)
+{
+const prompt = require ("prompt-sync")();
+let digitadonome = prompt ('Digite os nome: ');
+}
+console.log ('os nomes armazenados foram ' + digitadonome)
